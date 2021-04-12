070f1d6f16ac16eee774e809c08d573e71e03caf
lathamjohnson <theljohnson20@gmail.com>
2021-04-12 10:02:46 -0400

    updated to public

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var pad = document.querySelectorAll('.pad')
 const newGame = document.querySelector('#newGame')
 const levelCount = document.querySelector('#levelCount')
-var level = 3
+var recording
+var level = 1
 var playerMove = 0
 var playerString = ''
 var computerMove = 0
 var computerString = ''
-var recording
 var currentSequence = setInterval(recordPad, 1000, 0)
 clearInterval(currentSequence)
 
@@ -17,9 +17,9 @@ pad.forEach(p => {
 
 newGame.addEventListener('click', startSequence)
 
-function startSequence(){
-    reset(3)
-    currentSequence = setInterval(recordPad, 1500)
+function startSequence(click=false){
+    if(click){reset(1)}
+    currentSequence = setInterval(recordPad, 1200)
 }
 
 function setStatus(message){document.querySelector('#status').innerHTML = message}
@@ -29,11 +29,11 @@ function deactivate(pad, status='on'){pad.classList.remove(status)}
 function animate(pad){
     if(pad.isTrusted && recording == false){
     activate(pad.toElement)
-    setTimeout(deactivate, 200, pad.toElement)       
+    setTimeout(deactivate, 150, pad.toElement)       
     }
     else{
     activate(pad)
-    setTimeout(deactivate, 1000, pad)
+    setTimeout(deactivate, 800, pad)
     }
 }
 
@@ -46,10 +46,8 @@ function scorePlayer(e){
     }
     else if(playerString === computerString){
         setStatus('Correct! Next level...')
-        reset()
-        level ++
-        levelCount.innerText = `Level: ${level-2}`
-        setTimeout(setStatus, 1000, `Level: ${level - 2}` )
+        reset(++level)
+        levelCount.innerText = `Level: ${level}`
         startSequence()
     }
     else{
@@ -59,7 +57,8 @@ function scorePlayer(e){
 }
 
 function recordPad(){
-    if(computerMove==level){
+    if(computerMove==(level)){
+        console.log(computerString)
         clearInterval(currentSequence)
         recording = false
         setTimeout(setStatus, 500, `Repeat it!`)
@@ -75,7 +74,7 @@ function recordPad(){
     }
 }
 
-function reset(l=level){
+function reset(l){
     playerMove = 0
     playerString = ''
     computerMove = 0
@@ -84,7 +83,7 @@ function reset(l=level){
     pad.forEach(p =>{
         deactivate(p, 'wrong')
     })
-    levelCount.innerText = `Level: ${level-2}`
+    levelCount.innerText = `Level: ${level}`
 }
 
 function endGame(){
@@ -95,8 +94,11 @@ function endGame(){
     setTimeout(setStatus, 1500, 'Click above to start a new game')
 }
 
-
-//Pad Check Function
-//Is called each time the player hits a pad
-//Takes the value of the correct pad sequence, checks against it every time player hits a pad
-//May use incrementation to check through the correct value, or hold in the same data type and compare
+function printInfo(info){
+    console.log(info)
+    console.log(`playerMove: ${playerMove}`)
+    console.log(`playerString: ${playerString}`)
+    console.log(`computerMove: ${computerMove}}`)
+    console.log(`computerString: ${computerString}}`)
+    console.log(`Level: ${level}`)
+    }
