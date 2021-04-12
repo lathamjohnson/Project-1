4eea454295e2b42a983939d2b6472ea0a5030425
lathamjohnson <theljohnson20@gmail.com>
2021-04-12 17:02:49 -0400

    improved animation and styling, added score tracker and difficulty selector

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var pad = document.querySelectorAll('.pad')
 const newGame = document.querySelector('#newGame')
 const levelCount = document.querySelector('#levelCount')
-var recording
+const difficulty = document.querySelector('#difficulty')
+const board = document.querySelector('.board')
+const score = document.querySelector('#score')
+var gameSpeed = 1200
+var recording = true
 var level = 1
 var playerMove = 0
 var playerString = ''
 var computerMove = 0
 var computerString = ''
+
+score.innerText = document.cookie.substring(10)
+
 var currentSequence = setInterval(recordPad, 1000, 0)
 clearInterval(currentSequence)
 
@@ -15,11 +22,29 @@ pad.forEach(p => {
     p.addEventListener('click', animate)
 })
 
+
+difficulty.addEventListener('click', setSpeed)
 newGame.addEventListener('click', startSequence)
+
+
+function rankScore(cookie){
+    const docScore = parseInt(document.cookie.substring(10), 10)
+    const score = parseInt(cookie.substring(10), 10)
+    if(score > docScore){document.cookie = `highscore=${score}`}
+    else if(score < docScore){return}
+    else{document.cookie = cookie}
+}
+
+function setSpeed(e){
+    if(e.target.id == 'easy'){gameSpeed = 1200}
+    else if(e.target.id == 'medium'){gameSpeed = 600}
+    else if(e.target.id == 'hard'){gameSpeed = 300}
+    else{return}
+}
 
 function startSequence(click=false){
     if(click){reset(1)}
-    currentSequence = setInterval(recordPad, 1200)
+    currentSequence = setInterval(recordPad, gameSpeed)
 }
 
 function setStatus(message){document.querySelector('#status').innerHTML = message}
@@ -33,7 +58,7 @@ function animate(pad){
     }
     else{
     activate(pad)
-    setTimeout(deactivate, 800, pad)
+    setTimeout(deactivate, (gameSpeed/2), pad)
     }
 }
 
@@ -42,6 +67,7 @@ function scorePlayer(e){
     else{
     playerString += e.toElement.id.charAt(3)
     if(playerString.charAt(playerMove) !== computerString.charAt(playerMove)){
+        rankScore(`highscore=${level-1}`)
         endGame()
     }
     else if(playerString === computerString){
@@ -58,7 +84,6 @@ function scorePlayer(e){
 
 function recordPad(){
     if(computerMove==(level)){
-        console.log(computerString)
         clearInterval(currentSequence)
         recording = false
         setTimeout(setStatus, 500, `Repeat it!`)
@@ -75,21 +100,20 @@ function recordPad(){
 }
 
 function reset(l){
+    console.log(document.cookie)
     playerMove = 0
     playerString = ''
     computerMove = 0
     computerString = ''
     level = l
-    pad.forEach(p =>{
-        deactivate(p, 'wrong')
-    })
+    deactivate(board, 'wrong')
     levelCount.innerText = `Level: ${level}`
 }
 
 function endGame(){
-    pad.forEach(p => {
-        activate(p, 'wrong')
-    })
+    recording=true
+    score.innerText = document.cookie.substring(10)
+    activate(board, 'wrong')
     setStatus('Thats not what simon said...')
     setTimeout(setStatus, 1500, 'Click above to start a new game')
 }
