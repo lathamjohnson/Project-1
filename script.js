0b80d4740f78deabf09323763373e71a1a82ff73
lathamjohnson <theljohnson20@gmail.com>
2021-04-13 21:21:25 -0400

    pages tweak

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ function activate(pad, status='on'){pad.classList.add(status)}
 function deactivate(pad, status='on'){pad.classList.remove(status)}
 
 function checkMute(){
-    if(muted){muted=false; mute.src='Project-1/assets/unmuted.png'}
-    else{muted=true; mute.src='Project-1/assets/muted.png'}
+    if(muted){muted=false; mute.src='assets/unmuted.png'}
+    else{muted=true; mute.src='assets/muted.png'}
 }
 
 function playSound (element){
